b5bb281380d8e1ee73c63925b98b10393e70c615
TheSyava <levkosvatoslav@gmail.com>
2025-05-30 02:28:20 +0300

    news-fix

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ function init() {
     import('./index.faq.js'); // Dmytro Gerasymenko
     import('./global.brand-carousel.js'); // Dmytro Belevtsev
     import('./index.contact-us.js'); //Anastasiia Dubachynska
-    import('./index.news.js'); // Levko Sviayoslav
+    import('./news-scroll.js'); // Levko Sviayoslav
 }
 
 const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
