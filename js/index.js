66263dc08b331c7c169b62c51c1da60cd0e510e4
AnastasiiaDb <anastasiia.dubachynska@gmail.com>
2025-05-29 22:44:27 +0000

    fix: slider & add form fetch;

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ function init() {
     import('./index.cases.js'); // Anastasiia Dubachynska
     import('./index.faq.js'); // Dmytro Gerasymenko
     import('./global.brand-carousel.js'); // Dmytro Belevtsev
+    import('./index.contact-us.js'); //Anastasiia Dubachynska
 }
 
 const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
