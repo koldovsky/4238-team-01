51169bafae97ad2064d5506df95108977d08b3eb
belevtsev-dm <belevtsev.dmytro@gmail.com>
2025-05-30 15:46:52 +0300

    about-us.history js added

--- js/about-us.js
+++ js/about-us.js
@@ -1,5 +1,6 @@
 function init() {
     import('./global.brand-carousel.js'); // Dmytro Belevtsev
+    import('./about-us.history.js'); // Dmytro Belevtsev
 }
 
 const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
